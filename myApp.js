f9574b0c3fd922e8ba19ca68a7583d46b763b0cf
Michael Tseng <matseng@gmail.com>
2014-05-28 14:09:51 -0700

    Able to highlight row/col of mouseover element by setting activeRow and activeCol, then digest cycle iterates over each table cell to determine ng-class assignment

--- myApp.js
+++ myApp.js
@@ -18,7 +18,17 @@ angular.module('myApp')
     $scope.$watch('numberLimit', function(newNumberLimit) {
       $scope.numbers = populateNumbers(newNumberLimit);
     });
-
     $scope.numberLimit = $attrs.initialNumberLimit || 10;
+
+    var activeRow, activeCol;
+    $scope.setActiveFactors = function(row, col) {
+      activeRow = row;
+      activeCol = col;
+    };
+
+    $scope.matchesFactor = function(row, col) {
+      return row === activeRow || col === activeCol;
+    };
+
 
   }]);
