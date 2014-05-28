2b40c84b438103df4b8b39bbc8fe1c82ab89b387
Michael Tseng <matseng@gmail.com>
2014-05-28 13:46:23 -0700

    Moved controller to div, added  to ng-model of input element

--- myApp.js
+++ myApp.js
@@ -2,8 +2,6 @@ angular.module('myApp', []);
 
 angular.module('myApp')
   .controller('MultiplcationController', ['$scope', '$attrs', function($scope, $attrs) {
-    // $scope.numbers = [1,2,3,4,5,6,7,8,9,10];
-    $attrs.initialNumberLimit;
 
     function populateNumbers(x) {
       var numbers = [];
@@ -12,8 +10,15 @@ angular.module('myApp')
       };
       return numbers;
     }
+
+    $scope.compute = function(a, b) {
+      return a * b;
+    };
+
+    $scope.$watch('numberLimit', function(newNumberLimit) {
+      $scope.numbers = populateNumbers(newNumberLimit);
+    });
+
     $scope.numberLimit = $attrs.initialNumberLimit || 10;
-    $scope.numbers = populateNumbers($scope.numberLimit);
-    console.log($scope.numbers);
-    
+
   }]);
